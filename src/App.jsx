44e12a949bae0d6999a0357cc6a6c5c580beabfc
arwaUnox <arwa.fahoud@unox.com>
2025-04-12 23:11:38 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar';
 //this is the app for testing sonarqube with github actions
 const App = () => {
   const { token } = useAuthContext();
+  return null
   return (
     <>
       <div className='fixed bottom-0 left-0 top-0'>
