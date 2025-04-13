175ece011e2e4a549f51653ff1663775e80a3102
arwaUnox <arwa.fahoud@unox.com>
2025-04-13 11:01:25 +0200

    add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
